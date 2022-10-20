b808a7fd98166e525adefab826444f0bc0c54296
Saylana <lanakhomushku@gmail.com>
2022-10-20 12:00:41 +0300

    db done

---
+++ server/db/migrations/20221020083045-create-device.js
@@ -0,0 +1,39 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Devices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      room_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Rooms',
+          },
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Devices');
+  },
+};
