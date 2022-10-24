a22c83936171a3083bc82dabbe70beb274544501
Saylana <lanakhomushku@gmail.com>
2022-10-24 09:43:35 +0300

    addLoc

--- client/src/Components/AddLocation/AddLocation.jsx
+++ client/src/Components/AddLocation/AddLocation.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import DragAndDrop from '../../components/DragAndDrop/DragAndDrop';
+import DropLocation from '../DropLocation/DropLocation';
 
 // jcef
 export default function AddLocation() {
   return (
     <div>
-      <div className="form__group field">
-        <input type="text" className="form__field" placeholder="New Location" name="title" />
-        <label htmlFor="name" className="form__label">New Location</label>
-      </div>
-      <div />
-      <DragAndDrop />
+      <DropLocation />
     </div>
   );
 }
