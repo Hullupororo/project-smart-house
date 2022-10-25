2cae9999a6dda00b8c7e2a07668be57bcaa578e2
Darya <dariaavs@gmail.com>
2022-10-25 10:31:14 +0300

    outlet card done

--- client/src/Components/ManageDevice/Outlet/ManageOutlet.jsx
+++ client/src/Components/ManageDevice/Outlet/ManageOutlet.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './outlet.css';
+import { RiOutlet2Fill } from 'react-icons/ri';
 
 export default function ManageOutlet() {
   return (
-    <div>ManageOutlet</div>
+    <div className="DashaProMax">
+      <div className="outletSettings">
+        <div className="logo">
+          <RiOutlet2Fill />
+          <h3 className="header">Outlet</h3>
+        </div>
+        <div>
+          <h5 className="forMargin">On / Off</h5>
+          <label className="switch">
+            <input type="checkbox" />
+            <div>
+              <span />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
   );
 }
