3945a8b6ac27f454e50791bbebe01933b594d8c6
Darya <dariaavs@gmail.com>
2022-10-26 15:25:31 +0300

    telegram bot done

--- server/bot/index.js
+++ server/bot/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const TelegramApi = require('node-telegram-bot-api');
 const express = require('express');
 const axios = require('axios');
@@ -9,34 +10,61 @@ const token = '5476046874:AAH6xdcFi8CQWdOOX_NeQRXSqQR4IgYvLRY';
 const bot = new TelegramApi(token, { polling: true });
 
 const start = async () => {
-  bot.setMyCommands([
-    { command: '/start', description: 'Start' },
-    { command: '/lampon', description: 'Turn Lamp On' },
-    { command: '/lampoff', description: 'Turn Lamp Off' },
-  ]);
+//   bot.setMyCommands([
+//     { command: '/start', description: 'Start' },
+//     { command: '/lampon', description: 'Turn Lamp On' },
+//     { command: '/lampoff', description: 'Turn Lamp Off' },
+//   ]);
 
   bot.on('message', async (msg) => {
     const { text } = msg;
     const chatId = msg.chat.id;
+    // console.log(msg);
 
     try {
       if (text === '/start') {
-        return bot.sendMessage(chatId, 'Welcome to Smart House ');
+        // return bot.sendMessage(chatId, 'Welcome to Smart House. Choose one of the following commands in Menu ');
+        return bot.sendMessage(msg.chat.id, 'Welcome to Smart House. Choose one of the following commands in Menu', {
+          reply_markup: {
+            keyboard: [['Turn Lamp On', 'Turn Lamp Off'], ['Turn Socket On', 'Turn Socket Off']],
+          },
+        });
       }
 
-      if (text === '/lampon') {
+      if (text === 'Turn Lamp On') {
         axios.get('http://localhost:3001/dev/funconbot')
           .then((res) => {
             if (res.data === 'OK') {
-              return bot.sendMessage(chatId, 'Lamp is on');
+              return bot.sendMessage(chatId, 'Lamp is On');
             }
           });
       }
-      if (text === '/lampoff') {
+      if (text === 'Turn Lamp Off') {
         axios.get('http://localhost:3001/dev/funcoffbot')
           .then((res) => {
             if (res.data === 'OK') {
-              return bot.sendMessage(chatId, 'Lamp is off');
+              return bot.sendMessage(chatId, 'Lamp is Off');
+            }
+          });
+      }
+    } catch (e) {
+      return bot.sendMessage(chatId, 'Something went wrong!');
+    }
+
+    try {
+      if (text === 'Turn Socket On') {
+        axios.get('http://localhost:3001/dev/outletonbot')
+          .then((res) => {
+            if (res.data === 'OK') {
+              return bot.sendMessage(chatId, 'Socket is On');
+            }
+          });
+      }
+      if (text === 'Turn Socket Off') {
+        axios.get('http://localhost:3001/dev/outletoffbot')
+          .then((res) => {
+            if (res.data === 'OK') {
+              return bot.sendMessage(chatId, 'Socket is Off');
             }
           });
       }
--- server/routes/funcRoute.js
+++ server/routes/funcRoute.js
@@ -76,5 +76,27 @@ router.get('/outleton', (req, res) => {
   });
   res.sendStatus(200);
 });
+router.get('/outletonbot', (req, res) => {
+  const client = mqtt.connect('mqtt://192.168.2.155:1883');
+  client.on('connect', () => {
+    client.subscribe('presence', (err) => {
+      if (!err) {
+        client.publish('zigbee2mqtt/Outlet/set', '{"state": "ON"}');
+      }
+    });
+  });
+  res.sendStatus(200);
+});
+router.get('/outletoffbot', (req, res) => {
+  const client = mqtt.connect('mqtt://192.168.2.155:1883');
+  client.on('connect', () => {
+    client.subscribe('presence', (err) => {
+      if (!err) {
+        client.publish('zigbee2mqtt/Outlet/set', '{"state": "OFF"}');
+      }
+    });
+  });
+  res.sendStatus(200);
+});
 
 module.exports = router;
